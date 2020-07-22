94ed3fa31ba5b436872e7e8781fb01b075d74a4f
Samson303 <samson.mulugeta@code.berlin>
2020-07-22 14:26:07 +0200

    feat: State and Life Cycle

--- src/App.js
+++ src/App.js
@@ -1,17 +1,63 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "./App.css";
 import WeatherCard from "./components/WeatherCard/component";
 
 function App() {
+  const [city, setCity] = useState("Sydney, au");
+  const [weather, setWeather] = useState({
+    temp: null,
+    city: null,
+    condition: null,
+    country: null,
+  });
+  const data = async () => {
+    const apiRes = await fetch(
+      `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=011c91df86120d2410af605e3830366b`
+    );
+    const resJSON = await apiRes.json();
+    return resJSON;
+  };
+
+  const handleSearch = (e) => {
+    e.preventDefault();
+    data().then((res) => {
+      setWeather({
+        temp: res.main.temp,
+        city: res.name,
+        condition: res.weather[0].main,
+        country: res.country,
+      });
+    });
+  };
+
+  useEffect(() => {
+    data().then((res) => {
+      setWeather({
+        temp: res.main.temp,
+        city: res.name,
+        condition: res.weather[0].main,
+        country: res.country,
+      });
+    });
+  }, []);
   return (
     <div className="App">
       <WeatherCard
-        temp={35}
-        condition="Clear"
-        city="Sydny"
-        country="Australia"
+        temp={weather.temp}
+        condition={weather.condition}
+        city={weather.city}
+        country={weather.country}
       />
-      <WeatherCard temp={15} condition="Clouds" city="London" country="UK" />
+      <form action="">
+        <input
+          type="text"
+          value={city}
+          onChange={(e) => setCity(e.target.value)}
+        />
+        <button type="submit" onClick={(e) => handleSearch(e)}>
+          Search
+        </button>
+      </form>
     </div>
   );
 }
